284736abf9329f95c55fa87796d1b8ea31194b3e
Saad bis Usman <test@saad.com>
2017-05-27 00:57:11 +0500

    added hello world program

---
+++ firstsession/firstclass.java
@@ -0,0 +1,23 @@
+package firstsession;
+
+
+
+public class firstclass {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		
+			System.out.println("hello");
+			System.out.println("hello 1");
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
